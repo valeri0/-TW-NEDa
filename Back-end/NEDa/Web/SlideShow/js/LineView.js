94811219421a864a8156db1796ec815c3d0a3d4a
Mihaila Valerio <mihaila.andrei95@gmail.com>
2017-05-17 20:17:49 +0300

    Fully working LineView

--- Back-end/NEDa/Web/SlideShow/js/LineView.js
+++ Back-end/NEDa/Web/SlideShow/js/LineView.js
@@ -2,128 +2,107 @@
  * Created by Ovidiu on 07-Apr-17.
  */
 google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(initialize);
 
-var parameters = [0,99999999999,0,10000000];
+
+
+    function getData(min_fatalities,max_fatalities,min_magnitude,max_magnitude,cb){
+
+        var getRequest = new XMLHttpRequest();
+
+        getRequest.open('GET',`http://localhost:8081/earthquakes/search?minFatalities=${min_fatalities}&maxFatalities=${max_fatalities}&minMagnitude=${min_magnitude}&maxMagnitude=${max_magnitude}`)
+
+        getRequest.onload = function(){
+            var object = JSON.parse(getRequest.responseText);
+            cb(object);
+        };
+    
+        getRequest.send();
+    }
 
 function initialize(){
 
     addEventHandlers();
 
-    getData(parameters[0],parameters[1],parameters[2],parameters[3]);
+    var parameters = [0,99999999999,0,10000000];
 
-    drawChart();
-}
-
-function getData(min_fatalities,max_fatalities,min_magnitude,max_magnitude,cb){
-
-    var getRequest = new XMLHttpRequest();
-
-    getRequest.open('GET',`http://localhost:8081/earthquakes/search?minFatalities=${min_fatalities}&maxFatalities=${max_fatalities}&minMagnitude=${min_magnitude}&max_magnitude=${max_magnitude}`)
-
-    getRequest.onload = function(){
-        var object = JSON.parse(getRequest.responseText);
-        console.log(object);
-        cb(object);
-    };
+    getData(parameters[0],parameters[1],parameters[2],parameters[3],drawChart);
     
-    getRequest.send();
-}
 
-function initData(data) {
-
-    console.log(data.fatalities);
-}
-
-function drawChart() {
+    function drawChart(data) {
 
 
-    var data_magnitude = new google.visualization.DataTable();
-    data_magnitude.addColumn('date', 'Date of earthquake');
-    data_magnitude.addColumn('number', 'Magnitude');
+        
 
-    data_magnitude.addRows([
+        var data_magnitude = new google.visualization.DataTable();
+        data_magnitude.addColumn('date', 'Date of earthquake');
+        data_magnitude.addColumn('number', 'Magnitude');
 
-        [new Date(2000, 8, 25),5],
-        [new Date(2002, 12, 3),8],
-        [new Date(2005, 2, 10), 10],
-        [new Date(2007, 9, 5), 5],
-        [new Date(2008, 3, 15),12],
-        [new Date(2010, 8, 15),13]
-
-    ]);
+        for(var i = 0; i < data.length; i++){
+            var obj = data[i];
+            data_magnitude.addRow([new Date(obj.earthquakeDate),obj.magnitude]);
+        }
 
 
-    var data_deaths = new google.visualization.DataTable();
-    data_deaths.addColumn('date','Date of the earthquake');
-    data_deaths.addColumn('number','Deaths');
-    data_deaths.addRows([
-
-        [new Date(2000, 8, 25),1000],
-        [new Date(2002, 12, 3),1500],
-        [new Date(2005, 2, 10), 1200],
-        [new Date(2007, 9, 5), 800],
-        [new Date(2008, 3, 15),900],
-        [new Date(2010, 8, 15),215]
-
-    ]);
+        var data_deaths = new google.visualization.DataTable();
+        data_deaths.addColumn('date','Date of the earthquake');
+        data_deaths.addColumn('number','Deaths');
+        for(var i = 0; i < data.length; i++){
+            var obj = data[i];
+            data_deaths.addRow([new Date(obj.earthquakeDate),obj.fatalities]);
+        }
 
 
-    var export_deaths_png = document.getElementById("export_deaths_png");
-    var export_magnitude_png = document.getElementById("export_magnitude_png");
+        var export_deaths_png = document.getElementById("export_deaths_png");
+        var export_magnitude_png = document.getElementById("export_magnitude_png");
 
 
-    var magnitude_options = {
-        colors:['#7f440a'],
-        curveType: 'function',
-        hAxis: {
-            title: 'Earthquake Date'
-        },
-        vAxis: {
-            title: 'Magnitude'
-        },
-        animation:{"startup": true, duration: 1300, easing: 'out'},
-    };
+        var magnitude_options = {
+            colors:['#7f440a'],
+            curveType: 'function',
+            hAxis: {
+                title: 'Earthquake Date'
+            },
+            vAxis: {
+                title: 'Magnitude'
+            },
+            animation:{"startup": true, duration: 1300, easing: 'out'},
+        };
 
-    var deaths_options = {
-        colors:['#ff100c'],
-        curveType: 'function',
-        hAxis: {
-            title: 'Earthquake Date'
-        },
-        vAxis: {
-            title: 'Deaths'
-        },
-        animation:{"startup": true, duration: 1300, easing: 'out'},
-    };
-
-
-    var first_chart = new google.visualization.LineChart(
-        document.getElementById('chart_magnitude'));
-
-    var second_chart = new google.visualization.LineChart(
-        document.getElementById('chart_deaths'));
-
-    google.visualization.events.addListener(first_chart,'ready',function(){
-
-        export_magnitude_png.innerHTML='<a href="' + first_chart.getImageURI() + '" download="chart_magnitude"  class="btn btn-primary " role="button">Export as PNG</a>';
-    });
-
-    google.visualization.events.addListener(second_chart,'ready',function(){
-        export_deaths_png.innerHTML='<a href="' + second_chart.getImageURI() + '" download="chart_deaths"  class="btn btn-primary " role="button">Export as PNG</a>';
-
-    });
-
-    first_chart.draw(data_magnitude, magnitude_options);
-    second_chart.draw(data_deaths,deaths_options);
+        var deaths_options = {
+            colors:['#ff100c'],
+            curveType: 'function',
+            hAxis: {
+                title: 'Earthquake Date'
+            },
+            vAxis: {
+                title: 'Deaths'
+            },
+            animation:{"startup": true, duration: 1300, easing: 'out'},
+        };
 
 
+        var first_chart = new google.visualization.LineChart(
+            document.getElementById('chart_magnitude'));
 
+        var second_chart = new google.visualization.LineChart(
+            document.getElementById('chart_deaths'));
 
+        google.visualization.events.addListener(first_chart,'ready',function(){
 
-}
+            export_magnitude_png.innerHTML='<a href="' + first_chart.getImageURI() + '" download="chart_magnitude"  class="btn btn-primary " role="button">Export as PNG</a>';
+        });
 
-function addEventHandlers () {
+        google.visualization.events.addListener(second_chart,'ready',function(){
+            export_deaths_png.innerHTML='<a href="' + second_chart.getImageURI() + '" download="chart_deaths"  class="btn btn-primary " role="button">Export as PNG</a>';
+
+        });
+
+        first_chart.draw(data_magnitude, magnitude_options);
+        second_chart.draw(data_deaths,deaths_options);
+    }
+
+    function addEventHandlers () {
 
 
 
@@ -134,6 +113,8 @@ function addEventHandlers () {
 
             parameters[0]=firstParam;
             parameters[1]=secondParam;
+
+            getData(parameters[0],parameters[1],parameters[2],parameters[3],drawChart);
 
         });
 
@@ -146,7 +127,11 @@ function addEventHandlers () {
             parameters[2]=firstParam;
             parameters[3]=secondParam;
 
+            getData(parameters[0],parameters[1],parameters[2],parameters[3],drawChart);
+
         });
+
+    }
 
 }
 
--- Back-end/NEDa/src/main/java/com/TWNEDa/Earthquakes/EarthquakesRepository.java
+++ Back-end/NEDa/src/main/java/com/TWNEDa/Earthquakes/EarthquakesRepository.java
@@ -13,7 +13,7 @@ import java.util.List;
 public interface EarthquakesRepository extends JpaRepository<Earthquake,Integer>{
 
 
-    @Query("SELECT e from Earthquake e where (e.fatalities between ?1 and ?2) and (e.magnitude between ?3 and ?4)")
+    @Query("SELECT e from Earthquake e where (e.fatalities between ?1 and ?2) and (e.magnitude between ?3 and ?4) order by e.earthquakeDate")
     public List<Earthquake> getFilteredEarthquakes(Long minFatalities,Long maxFatalities,Double minMagnitude,Double maxMagnitude);
 
 }
